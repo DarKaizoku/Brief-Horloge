7b5940390283c07fcbb3c589975944b1015a323d
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2022-11-18 15:27:39 +0100

    final fonctionnement de l'horloge

--- js/script.js
+++ js/script.js
@@ -5,10 +5,6 @@ const AIGUILLESEC = document.querySelector("#second");
 
 //Extraire l'heure actuel à l'aide de l'objet Date()
 
-let date = new Date();
-
-let heure = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-
 //Stocker l'heure , minute , seconde  dans des variables
 
 //Stocker l'heure , minute , seconde  dans des varaiables
@@ -19,36 +15,19 @@ let heure = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 // Déplacer les aiguilles
 
 function demarrerLaMontre() {
-    let date = new Date();
-    const hours = ((date.getHours() + 11) % 12 + 1 );
-    const minutes = date.getMinutes();
-    const seconds= date.getSeconds();
-    const hourDeg = hours * 30 ;       //360/12
-    const minuteDeg = minutes * 6; /* 360degrés / 60 minutes = 6 */
-    const secondDeg = seconds * 6;
-    console.log(date, hours, minutes, seconds,hourDeg,minuteDeg,secondDeg);
-
+  let date = new Date();
+  const hours = ((date.getHours() + 11) % 12) + 1;
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const hourDeg = hours * 30; //360/12
+  const minuteDeg = minutes * 6; /* 360degrés / 60 minutes = 6 */
+  const secondDeg = seconds * 6;
+  console.log(date, hours, minutes, seconds, hourDeg, minuteDeg, secondDeg);
+  AIGUILLEHR.style.transform = `rotate(${hourDeg}deg)`;
+  AIGUILLEMIN.style.transform = `rotate(${minuteDeg}deg)`;
+  AIGUILLESEC.style.transform = `rotate(${secondDeg}deg)`;
 }
 // Exercuter la fonction chaque second
-var interval = setInterval(demarrerLaMontre, 1000);
-
-//recursivité
-document.querySelector("#hour").style.transform = `rotate(${hour}deg)`;
-document.querySelector("#minute").style.transform = `rotate(${minute}deg)`;
-document.querySelector("#second").style.transform = `rotate(${second}deg)`;
-    let direct = new Date();
-      let sec = direct.getSeconds();
-      let min = direct.getMinutes();
-      let hour = direct.getHours();
-      console.log(direct);
-    };
 
 var interval = setInterval(demarrerLaMontre, 1000);
-
-
-
-
 // Exercuter la fonction chaque second
-
-
-//recursivité
